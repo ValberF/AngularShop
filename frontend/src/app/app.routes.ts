428e515c09ca2e2a49f3edbbc781e3baaa920e9a
joaopedropinto <32523778+joaopedropinto@users.noreply.github.com>
2024-05-27 17:18:26 -0300

    adding cart component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { ProductRegisterComponent } from './pages/product-register/product-register.component';
 import { ProductsComponent } from './pages/products/products.component';
+import { CartComponent } from './pages/cart/cart.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'product-register', component: ProductRegisterComponent },
-    { path: 'products', component: ProductsComponent }
+    { path: 'products', component: ProductsComponent },
+    { path : 'cart', component: CartComponent }
 
 ];
